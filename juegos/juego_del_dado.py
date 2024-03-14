9463c52eb1f03ef3ae1954a689fe010e77b934d8
ascordova <162610934+ascordova@users.noreply.github.com>
2024-03-14 19:28:26 +0000

    juegodadosfinal

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -6,6 +6,7 @@ def juego_del_dado():
     Después el computador también tiene que lanzar un dado.
     El primero en sumar 30 puntos gana.
     """
+    
     puntuacion_usuario = 0
     puntuacion_computador = 0
     while puntuacion_usuario < 30 or puntuacion_computador < 30:
